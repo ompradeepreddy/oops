c3232ec07ab84123577652621fb8a161c3de443c
ompradeepreddy <mr.dpureddy@gmail.com>
2025-05-28 12:10:18 +0530

    print all in class

---
+++ Classes-Objects/classes1.py
@@ -0,0 +1,10 @@
+class instructor:
+  def __init__(self,name,address):
+    self.name=name
+    self.address=address
+instructor_1=instructor("ram","ap")
+instructor_2=instructor("raj","up")
+print(instructor_1.name)
+print(instructor_1.address)
+print(instructor_2.name)
+print(instructor_2.address)
