8f9d644c93c4e9d1e27f5f6a65408c5965efe29a
ompradeepreddy <mr.dpureddy@gmail.com>
2025-05-28 12:19:11 +0530

    in class

---
+++ Classes-Objects/inclass1.py
@@ -0,0 +1,16 @@
+class instructor:
+  followers=0
+  def __init__(self,name,address):
+    self.name=name
+    self.address=address
+  def display(self,subject_name):
+    print(f"Hello i am {self.name} i will teach {subject_name}")
+  def update_followers(self,followers_name):
+    self.followers+=1
+instructor_1=instructor("ram","ap")
+instructor_2=instructor("raj","up")
+print(instructor_1.name)
+instructor_1.display("python")
+instructor_1.update_followers("raj")
+print(instructor_1.followers)
+print(instructor_2.followers)
