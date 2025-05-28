550b454f632dd7d54e8a5db2a0be6365ef6efda5
ompradeepreddy <mr.dpureddy@gmail.com>
2025-05-28 12:28:24 +0530

    area of circle in function

---
+++ Exercise-classes-obj/circle-area1-circle.py
@@ -0,0 +1,12 @@
+class circle:
+  pi=3.14
+  def __init__(self,radius):
+    self.radius=radius
+    self.area=circle.pi * radius*radius    #area of circle
+  def circumference_circle(self):
+    return 2* circle.pi *self.radius
+  #def area_circle(self):
+   # return circle.pi * (self.radius)**2
+circle_1=circle(5)
+print(circle_1.circumference_circle())
+print(circle_1.area)
